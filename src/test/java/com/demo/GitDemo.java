5814791c5f9f97e01f26779baab0e42c3a6fbe7c
yokesh-ak <yokesh.ananthakrishnan@gmail.com>
2024-07-31 12:54:07 +0530

    Added the filr fisrst time

---
+++ src/test/java/com/demo/GitDemo.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class GitDemo {
+	
+	private void methodA() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void methodB() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void methodC() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
