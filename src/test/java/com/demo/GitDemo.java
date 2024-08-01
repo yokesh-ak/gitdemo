4754d3f0d345002eb6194bd07c4530ce5e5d3f50
yokesh-ak <yokesh.ananthakrishnan@gmail.com>
2024-08-01 19:01:22 +0530

    Added new methods and a class

--- src/test/java/com/demo/GitDemo.java
+++ src/test/java/com/demo/GitDemo.java
@@ -16,5 +16,20 @@ public class GitDemo {
 		// TODO Auto-generated method stub
 
 	}
+	
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
